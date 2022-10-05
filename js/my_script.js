6d0b3a3c4647d0e287b4bdfe00589d402a9058bb
therealmensumit <sk03950@gmail.com>
2022-10-05 11:06:32 +0530

    changes in script

--- js/my_script.js
+++ js/my_script.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if( $(this).scrollTop() > 100 ) {
-            $('#header').addClass('fixed');
-        } else {
-            $('#header').removeClass('fixed');
-        }
-    });
+$(window).scroll(function(){
+    if( $(this).scrollTop() > 100 ) {
+        $('#header').addClass('fixed');
+    } else {
+        $('#header').removeClass('fixed');
+    }
+});
 
+$(document).ready(function(){
     $('.banner h5 a').click(function() {
         $('html, body').animate({
             scrollTop: $('.our-services').offset().top - 100
