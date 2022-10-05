aa4fce340a180955975b10dbf040409d0c8141cf
therealmensumit <sk03950@gmail.com>
2022-10-05 10:59:18 +0530

    scroll js

--- js/my_script.js
+++ js/my_script.js
@@ -6,13 +6,19 @@ $(document).ready(function(){
             $('#header').removeClass('fixed');
         }
     });
+
+    $('.banner h5 a').click(function() {
+        $('html, body').animate({
+            scrollTop: $('.our-services').offset().top - 100
+        }, 500)
+    });
 });
 
 wow = new WOW({
     animateClass: 'animated',
     offset: 100,
     callback: function (box) {
-        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        // console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
     }
 });
 wow.init();
